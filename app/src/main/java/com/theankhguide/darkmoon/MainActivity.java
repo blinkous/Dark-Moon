b45bbf9a84bd60fefc811081838b0669b2bea627
blinkous <saradhani1@gmail.com>
2019-08-21 15:32:35 -0400

    trying to implement swiping to navigate between tabs

--- app/src/main/java/com/theankhguide/darkmoon/MainActivity.java
+++ app/src/main/java/com/theankhguide/darkmoon/MainActivity.java
@@ -2,12 +2,15 @@ package com.theankhguide.darkmoon;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,21 +32,6 @@ public class MainActivity extends AppCompatActivity {
         /**Bottom Navigation*/
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         // Setting the listener for when something is selected in the bottom nav
-//        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-//            @Override
-//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//                switch (item.getItemId()) {
-//                    case R.id.action_apod:
-//                        // do something here
-//                        return true;
-//                    case R.id.action_other:
-//                        // do something here
-//                        onTapSearch();
-//                        return true;
-//                    default: return true;
-//                }
-//            }
-//        });
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -57,6 +45,32 @@ public class MainActivity extends AppCompatActivity {
                         return true;
                     default: return true;
                 }
+            }
+        });
+
+
+        /**Swipe gesture listeners*/
+//        View myView = (View) findViewById(R.id.container_view);
+        TextView myView = (TextView) findViewById(R.id.textTopTitle);
+        myView.setOnTouchListener(new OnSwipeTouchListener(this) {
+            @Override
+            public void onSwipeDown() {
+                Toast.makeText(MainActivity.this, "Down", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onSwipeLeft() {
+                Toast.makeText(MainActivity.this, "Left", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onSwipeUp() {
+                Toast.makeText(MainActivity.this, "Up", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onSwipeRight() {
+                Toast.makeText(MainActivity.this, "Right", Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -73,7 +87,9 @@ public class MainActivity extends AppCompatActivity {
             //Handle a successful response//
             @Override
             public void onResponse(Call<RetroNasa> call, Response<RetroNasa> response) {
-                loadDataList(response.body());
+                if(response.body() != null) {
+                    loadDataList(response.body());
+                }
             }
 
             //Handle execution failures//
@@ -92,14 +108,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void loadDataList (RetroNasa nasaList) {
-        // Get references to the objects in the layout
-        TextView textTitle = findViewById(R.id.textTitle);
-        TextView desc = findViewById(R.id.textDesc);
-        ImageView imageView = findViewById(R.id.imageView);
+        if(!nasaList.get_title().isEmpty() && !nasaList.get_explanation().isEmpty() && !nasaList.get_url().isEmpty()){
+            // Get references to the objects in the layout
+            TextView textTitle = findViewById(R.id.textTitle);
+            TextView desc = findViewById(R.id.textDesc);
+            ImageView imageView = findViewById(R.id.imageView);
 
-        // Setting the layout objects
-        textTitle.setText(nasaList.get_title());
-        desc.setText(nasaList.get_explanation());
-        Picasso.get().load(nasaList.get_url()).into(imageView);
+            // Setting the layout objects
+            textTitle.setText(nasaList.get_title());
+            desc.setText(nasaList.get_explanation());
+            Picasso.get().load(nasaList.get_url()).into(imageView);
+        }
     }
 }
